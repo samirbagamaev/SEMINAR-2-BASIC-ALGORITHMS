32a47c69fc512311882bc4e027adfc100f25b803
Samir Bagamaev <05ru4315@gmail.com>
2022-12-20 13:13:02 +0300

    Написали программу которая показывает наибольшую из двухзначного числа

---
+++ ConsoleApp_001/Program.cs
@@ -0,0 +1,28 @@
+﻿// НАПИШИТЕ ПРОГРАММУ, КОТОРАЯ ВЫВОДИТ СЛУЧАЙНОЕ ЧИСЛО ИЗ ОТРЕЗКА [10, 99]
+// И ПОКАЗЫВАЕТ НАИБОЛЬШУЮ ЦИФРУ ЧИСЛА
+
+// СЛУЧАЙНОЕ ЧИСЛО 
+ int number = 69;
+
+// ПЕРВАЯ ЦИФРА ЧИСЛА number | firstDigit = первая цифра 
+ int firstDigit = number / 10; // делим чтобы получить 1 цифру из 2-х значного числа
+
+// ВТОРАЯ ЦИФРА ЧИСЛА number | secondDigit = вторая цифра
+ int secondDigit = number % 10; //
+
+  Console.WriteLine($"Случайное число: {number}");
+
+  if (firstDigit > secondDigit)
+  {
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+  }
+
+  if (secondDigit > firstDigit)
+  {
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+  }
+
+  if (firstDigit == secondDigit)
+  {
+    Console.WriteLine("Цифры в числе равны");
+  }
