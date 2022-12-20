8a78f6f8f054413eb245701c9c7e271425f6929d
Samir Bagamaev <05ru4315@gmail.com>
2022-12-20 17:17:09 +0300

    Написали программу, которая принимает на вход 2 числа и выводит, является ли второе число кратным первому

---
+++ ConsoleApp_003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+
+Console.WriteLine ("Введите первое число");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+if (firstDigit%secondDigit == 0)
+{
+    Console.WriteLine ("Это кратное число");
+}
+else 
+{
+    Console.WriteLine ("Результат");
+    Console.WriteLine (firstDigit%secondDigit);
+}
+
